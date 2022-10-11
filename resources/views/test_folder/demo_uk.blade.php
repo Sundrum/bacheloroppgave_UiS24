604254cd9b24ce3e88277843f4a97d5292621daa
www-data <www-data@staging.portal.7sense.no>
2022-10-11 09:04:52 +0000

    Demo-site updated with language

--- resources/views/test_folder/demo_uk.blade.php
+++ resources/views/test_folder/demo_uk.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/scss" href="../resources/sass/app.scss">
+    <link rel="stylesheet" type="text/css" href="css/demo.css">
     <title>Demo UK</title>
 </head>
 <body>
@@ -158,7 +159,20 @@ const icons =
         src: url + "temperature.png",
         title: "Soil temperature",
         unit: "%"
+    },
+
+    flagNorway:
+    {
+        src: url + "nor-flag.png",
+        title: "NOR",
+    },
+
+    flagUK:
+    {
+        src: url + "uk-flag.png",
+        title: "UK",
     }
+    
 }
 
 /* Irrigation sensors */
@@ -402,6 +416,8 @@ const renderInfoButton = () =>
     <svg class="info-btn svg-inline--fa fa-info-circle fa-w-16 fa-3x fa-fw float-right" style="margin-right: 10px;" data-toggle="modal" data-target="#myInfowindow" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="info-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
         <path fill="#212529" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path>
     </svg>
+    <img class="mt-1 mb-0 mr-2 btn-flag-active image-responsive" src="${icons.flagUK.src}">
+    <img class="mt-1 mb-0 ml-2 btn-flag image-responsive" src="${icons.flagNorway.src}" onclick="window.location='demo_norway';">
     <div class="modal fade" id="myInfowindow" role="dialog" style="display: none;" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
