ac66480b0c6f7009c33de6d2b0a123e1ed04591a
www-data <www-data@staging.portal.7sense.no>
2023-04-20 08:44:34 +0000

    Users settings layout: fix

--- resources/views/pages/settings/accountsettings.blade.php
+++ resources/views/pages/settings/accountsettings.blade.php
@@ -3,8 +3,8 @@
 </div>
 
 <div class="row">
-    <div class="col-12">
-        <button class="btn btn-success card-rounded float-right" onclick="removeValues()" type="button" data-toggle="modal" data-target="#newUser">
+    <div class="col-12 text-end">
+        <button class="btn-7g" onclick="removeValues()" type="button" data-toggle="modal" data-target="#newUser">
             + @lang('settings.newuser')
         </button>
     </div>
@@ -14,72 +14,78 @@
     {{-- Foreach user --}}
     <div class="col-12">
         @foreach ($users as $user)
-            <div id="object{{$user->user_id}}" onclick="rotateImg('arrow{{$user->user_id}}')" class="row card card-rounded bg-light mt-4" data-toggle="collapse" data-target="#collapseuser{{$user->user_id}}" aria-expanded="true" aria-controls="collapseuser">
+            <div id="object{{$user->user_id}}" onclick="rotateImg('arrow{{$user->user_id}}')" class="card-rounded bg-white p-3" data-toggle="collapse" data-target="#collapseuser{{$user->user_id}}" aria-expanded="true" aria-controls="collapseuser">
                 <input type="hidden" id="user_name{{$user->user_id}}" value="{{$user->user_name}}">
                 <input type="hidden" id="user_email{{$user->user_id}}" value="{{$user->user_email}}">
                 <input type="hidden" id="user_phone_work{{$user->user_id}}" value="{{$user->user_phone_work}}">
                 <input type="hidden" id="roletype_id_ref{{$user->user_id}}" value="{{$user->roletype_id_ref}}">
 
-                <div class="row mt-3 mb-3">
-                    <div class="pl-5 col-10 ">
+                <div class="row">
+                    <div class="col-10">
                         <div class="row">
                             <h4 class="v-align"><strong>{{$user->user_name ?? ''}}</strong></h4>
                         </div>
                         <div class="row">
-                            {{$user->user_email ?? ''}}
+                            <span>{{$user->user_email ?? ''}}</span>
                         </div>
                     </div>
-                    <div class="col-2 align-self-center">
+                    <div class="col-2 align-self-center text-end">
                         <img id="arrow{{$user->user_id}}" data-toggle="collapse" data-target="#collapseuser{{$user->user_id}}" aria-expanded="true" aria-controls="collapseuser{{$user->user_id}}" src="{{ asset('img/expand.svg') }}">
                     </div>
                 </div>
             </div>
-            <div id="collapseuser{{$user->user_id}}" class="collapse bg-light" aria-labelledby="headingOne" data-parent="#object{{$user->user_id}}">
+            <div id="collapseuser{{$user->user_id}}" class="collapse bg-white col-10 offset-1" aria-labelledby="headingOne" data-parent="#object{{$user->user_id}}">
                 <div class="row justify-content-center">
-                    <button type="button" id="editUserDetails" onclick="userEdit({{$user->user_id}});" class="btn btn-primary card-rounded m-3" data-toggle="modal" data-target="#newUser">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"></path>
-                        </svg>
-                        @lang('settings.edituser')
-                    </button>
+                    <div class="col text-center">
+                        <button type="button" id="editUserDetails" onclick="userEdit({{$user->user_id}});" class="btn btn-primary card-rounded m-3" data-toggle="modal" data-target="#newUser">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"></path>
+                            </svg>
+                            @lang('settings.edituser')
+                        </button>
+                    </div>
                 </div>
-                <div class="container mt-4">
-                    @foreach($user->units as $unit)
-                        <div class="row mt-1 mb-1 card card-rounded" id="access{{$unit->sensoraccess_id}}">
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12 col-md-6 col-lg-5">
-                                        {{$unit->serialnumber ?? ''}} -> {{$unit->sensorunit_location ?? ''}}
-                                    </div>
-                                    <div class="col-12 col-md-6 col-lg-5">
-                                        @if($unit->changeallowed == '1')
-                                            @lang('settings.writeandread')
-                                        @else 
-                                            @lang('settings.readyonly')
-                                        @endif
-                                    </div>
-                                    <div class="col-12 col-lg-2">
-                                        <button type="button" class="btn btn-outline-danger card-rounded" onclick="deleteAccess({{$unit->sensoraccess_id}})">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                            </svg>
-                                            @lang('settings.deleteaccess')
-                                        </button>
+                <div class="m-1">
+                    <div class="col-12 mt-4">
+                        @foreach($user->units as $unit)
+                            <div class="row m-1 card card-rounded" id="access{{$unit->sensoraccess_id}}">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12 col-md-6 col-lg-5">
+                                            {{$unit->serialnumber ?? ''}} -> {{$unit->sensorunit_location ?? ''}}
+                                        </div>
+                                        <div class="col-12 col-md-6 col-lg-5">
+                                            @if($unit->changeallowed == '1')
+                                                @lang('settings.writeandread')
+                                            @else 
+                                                @lang('settings.readyonly')
+                                            @endif
+                                        </div>
+                                        <div class="col-12 col-lg-2">
+                                            <button type="button" class="btn btn-outline-danger card-rounded" onclick="deleteAccess({{$unit->sensoraccess_id}})">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                                </svg>
+                                                @lang('settings.deleteaccess')
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
                 <div class="row justify-content-center">
-                    <button type="button" class="btn btn-outline-danger card-rounded m-3" onclick="deleteUser({{$user->user_id}})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                        </svg>
-                        @lang('settings.deleteuser')
-                    </button>
+                    <div class="col text-center">
+                        <button type="button" class="btn btn-outline-danger card-rounded m-3" onclick="deleteUser({{$user->user_id}})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                            </svg>
+                            @lang('settings.deleteuser')
+                        </button>
+                    </div>
                 </div>
             </div>
         @endforeach
